80960c0f391330fd096a0f4b5bf1afa48b0aaa13
Alex761944 <christen.alexander@web.de>
2025-05-16 20:40:41 +0200

    Add copy and delete function (#5)

---
+++ script.js
@@ -0,0 +1,18 @@
+const copyButtonElement = document.querySelector("#copy-button");
+const removeButtonElement = document.querySelector("#remove-button");
+
+copyButtonElement.addEventListener("click", () => {
+  const normanCardElement = document.querySelector(".Section:has(.Norman)");
+  const normanCardCloneElement = normanCardElement.cloneNode(true);
+  normanCardElement.after(normanCardCloneElement);
+});
+
+removeButtonElement.addEventListener("click", () => {
+  const allNormanCardElements = document.querySelectorAll(
+    ".Section:has(.Norman)"
+  );
+
+  if (allNormanCardElements.length > 1) {
+    allNormanCardElements[allNormanCardElements.length - 1].remove();
+  }
+});
