4b9c6585a27070449044a094daf2cbb0574bda1b
Alex761944 <christen.alexander@web.de>
2025-05-19 14:37:32 +0200

    Add new chessboard with grid and javascript

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const copyButtonElement = document.querySelector('#copy-button');
-const removeButtonElement = document.querySelector('#remove-button');
+const copyButtonElement = document.querySelector("#copy-button");
+const removeButtonElement = document.querySelector("#remove-button");
 
-copyButtonElement.addEventListener('click', () => {
-  const normanCardElement = document.querySelector('.Section:has(.Norman)');
+copyButtonElement.addEventListener("click", () => {
+  const normanCardElement = document.querySelector(".Section:has(.Norman)");
   const normanCardCloneElement = normanCardElement.cloneNode(true);
   normanCardElement.after(normanCardCloneElement);
 });
 
-removeButtonElement.addEventListener('click', () => {
-  const allNormanCardElements = document.querySelectorAll('.Section:has(.Norman)');
+removeButtonElement.addEventListener("click", () => {
+  const allNormanCardElements = document.querySelectorAll(
+    ".Section:has(.Norman)"
+  );
 
   if (allNormanCardElements.length > 1) {
     allNormanCardElements[allNormanCardElements.length - 1].remove();
   }
 });
+
+const board = document.getElementById("chessboard");
+
+for (let row = 0; row < 8; row++) {
+  for (let col = 0; col < 8; col++) {
+    const square = document.createElement("div");
+    square.classList.add("Square");
+
+    if ((row + col) % 2 === 0) {
+      square.classList.add("WhiteSquare");
+    } else {
+      square.classList.add("BlackSquare");
+    }
+
+    board.appendChild(square);
+  }
+}
