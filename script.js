4a803e1c238753aea02de3b691edbd2991a5334f
Alex761944 <christen.alexander@web.de>
2025-05-22 19:59:17 +0200

    Add chessboard, google search and drawer (#9)

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-const copyButtonElement = document.querySelector('#copy-button');
-const removeButtonElement = document.querySelector('#remove-button');
+// Logic for copy buttons
 
-copyButtonElement.addEventListener('click', () => {
-  const normanCardElement = document.querySelector('.Section:has(.Norman)');
+const copyButtonElement = document.querySelector("#copy-button");
+const removeButtonElement = document.querySelector("#remove-button");
+
+copyButtonElement.addEventListener("click", () => {
+  const normanCardElement = document.querySelector(".Section:has(.Norman)");
   const normanCardCloneElement = normanCardElement.cloneNode(true);
   normanCardElement.after(normanCardCloneElement);
 });
 
-removeButtonElement.addEventListener('click', () => {
-  const allNormanCardElements = document.querySelectorAll('.Section:has(.Norman)');
+removeButtonElement.addEventListener("click", () => {
+  const allNormanCardElements = document.querySelectorAll(
+    ".Section:has(.Norman)"
+  );
 
   if (allNormanCardElements.length > 1) {
     allNormanCardElements[allNormanCardElements.length - 1].remove();
   }
+});
+
+// Logic for chessboard
+
+const boardElement = document.getElementById("chessboard");
+
+for (let row = 0; row < 8; row++) {
+  for (let col = 0; col < 8; col++) {
+    const squareElement = document.createElement("div");
+    squareElement.classList.add("Square");
+
+    if ((row + col) % 2 === 0) {
+      squareElement.classList.add("Square--White");
+    } else {
+      squareElement.classList.add("Square--Black");
+    }
+
+    boardElement.appendChild(squareElement);
+  }
+}
+
+// Logic for menu drawer
+
+const burgerElement = document.querySelector(".BurgerMenu");
+const drawerElement = document.querySelector(".Drawer");
+const closeButtonElement = drawerElement.querySelector(".CloseButton");
+
+burgerElement.addEventListener("click", () => {
+  drawerElement.classList.add("Drawer--Active");
+});
+
+closeButtonElement.addEventListener("click", () => {
+  drawerElement.classList.remove("Drawer--Active");
 });
