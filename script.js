87af3037f45208d477559778222d757a3402c5f6
Alex761944 <christen.alexander@web.de>
2025-05-30 19:14:51 +0200

    Make google search work (#13)

--- script.js
+++ script.js
@@ -136,3 +136,21 @@ removeCatButtonElement.addEventListener("click", () => {
     allCatImageElement.remove();
   });
 });
+
+// Logic for google search
+
+const googleInputElement = document.querySelector("#google-input");
+const googleSearchButtonElement = document.querySelector(
+  "#google-search-button"
+);
+
+googleSearchButtonElement.addEventListener("click", () => {
+  const googleInput = googleInputElement.value.trim();
+
+  if (googleInput !== "") {
+    const searchURL = `https://www.google.com/search?q=${encodeURIComponent(
+      googleInput
+    )}`;
+    window.open(searchURL, "_blank");
+  }
+});
