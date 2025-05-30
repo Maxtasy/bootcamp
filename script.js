8d0f925a3ef3ddf5e8e0ed45d38924cf563fc322
Alex761944 <christen.alexander@web.de>
2025-05-30 20:46:35 +0200

    Call with max (#15)

--- script.js
+++ script.js
@@ -293,3 +293,31 @@ deleteButtonElement.addEventListener("click", () => {
   calculator.delete();
   calculator.updateDisplay();
 });
+
+// Functions
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function randomGreeting(name) {
+  const greetings = ["Hello", "Hi", "Servus", "Good evening"];
+  const index = Math.round(Math.random() * 3);
+
+  return greetings[index] + ", " + name;
+}
+
+const result = randomGreeting("Elex");
+console.log(result);
