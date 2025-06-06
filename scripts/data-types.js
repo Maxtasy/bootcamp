580d18d5f33a9270b2b8e297815819da9a8d8e12
Alex761944 <christen.alexander@web.de>
2025-06-06 19:14:34 +0200

    Finish homework (#20)

---
+++ scripts/data-types.js
@@ -0,0 +1,60 @@
+// Booleans
+const falseBoolean = false;
+const oppositeOfFalseBoolean = !falseBoolean;
+const numberBoolean = 1;
+const stringBoolean = "";
+const undefinedBoolean = undefined;
+const nullBoolean = null;
+
+// Numbers
+const numberOne = 123;
+const decimalNumber = 10.5;
+
+// Strings
+const regularString = "Hallo";
+const templateString = `Hallo, ${numberOne}`;
+
+// Arrays
+const numberArray = [1, 2, 3, 4, 5, 6, 7];
+const arraySize = numberArray.length;
+const endIndex = arraySize - 1;
+// console.log(arraySize);
+
+let sum = 0;
+
+numberArray.forEach((item, index) => {
+  const rest = index % 2;
+  const isEvenNumber = rest === 0;
+
+  if (isEvenNumber) {
+    sum = sum + item;
+  } else {
+    sum = sum - item;
+  }
+});
+// console.log(sum);
+
+const result2 = numberArray[0] + numberArray[endIndex];
+// console.log(result2);
+
+const stringArray = ["a", "b", "c", "d"];
+stringArray.forEach((item, index) => {
+  if (item === "c") {
+    // console.log("i dont like c");
+  } else {
+    // console.log(item, index);
+  }
+});
+
+// Objects
+
+const object = {
+  a: 1,
+  b: ["Hallo", "Good Bye"],
+  c: stringArray,
+  d: {
+    x: "inside object d",
+  },
+};
+
+// console.log(object.d.x);
