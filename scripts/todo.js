ce7118bb93c5bea177951af52bb1cebc08b748f6
Maxtasy <maxtasy88@web.de>
2025-06-17 00:19:12 +0200

    Finish rework

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,5 +1,3 @@
-// Logic for ToDo section
-
 const toDoInputElement = document.querySelector("#to-do-input");
 const saveButtonElement = document.querySelector("#to-do-save-button");
 const toDoSectionElement = toDoInputElement.closest(".Section");
@@ -52,18 +50,17 @@ function addEventListenersToTodo(todoElement) {
     const currentToDos = getTodosFromLocalStorage();
     const isEditing = inputElement.classList.contains("ToDo__Input--Active");
 
+    const todoIndex = [...document.querySelectorAll(".ToDo")].findIndex(
+      (todo) => todo === todoElement,
+    );
+
     if (isEditing) {
-      const oldValue = textElement.textContent;
       const newValue = inputElement.value.trim();
 
       if (newValue !== "") {
         textElement.textContent = newValue;
 
-        const toDoIndex = currentToDos.findIndex(
-          (todo) => todo.text === oldValue,
-        );
-
-        currentToDos[toDoIndex].text = newValue;
+        currentToDos[todoIndex].text = newValue;
 
         localStorage.setItem(
           TODOS_LOCAL_STORAGE_KEY,
@@ -112,15 +109,21 @@ function appendToDo(toDoItem) {
     <input class="ToDo__Input Input" value="${toDoItem.text}" />
     <div class="IconGroup">
       <button class="CheckmarkButton Button--Icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-check-big-icon lucide-square-check-big"><path d="M21 10.656V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h12.344"/><path d="m9 11 3 3L22 4"/></svg>
+        <div class="Icon Icon--Success">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-check-big-icon lucide-square-check-big"><path d="M21 10.656V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h12.344"/><path d="m9 11 3 3L22 4"/></svg>
+        </div>
       </button>
 
       <button class="EditButton Button--Icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-pen-icon lucide-file-pen"><path d="M12.5 22H18a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v9.5"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M13.378 15.626a1 1 0 1 0-3.004-3.004l-5.01 5.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z"/></svg>
+        <div class="Icon Icon--Warning">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-pen-icon lucide-file-pen"><path d="M12.5 22H18a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v9.5"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M13.378 15.626a1 1 0 1 0-3.004-3.004l-5.01 5.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z"/></svg>
+        </div>
       </button>
 
       <button class="RemoveButton Button--Icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
+        <div class="Icon Icon--Destructive">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
+        </div>
       </button>
     </div>
   `;
