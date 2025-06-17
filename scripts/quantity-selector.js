8689878eb7451d74585602f788a9afeb24fc0fd6
Alex761944 <christen.alexander@web.de>
2025-06-17 22:09:28 +0200

    End call with max

--- scripts/quantity-selector.js
+++ scripts/quantity-selector.js
@@ -1,20 +1,34 @@
-const plusItemElement = document.querySelector(".QuantitySelectorItem--Plus");
-const valueItemElement = document.querySelector(".QuantitySelectorItem--Value");
-const minusItemElement = document.querySelector(".QuantitySelectorItem--Minus");
+const increaseButtonElement = document.querySelector(
+  ".QuantitySelector .Button:last-of-type",
+);
+const decreaseButtonElement = document.querySelector(
+  ".QuantitySelector .Button:first-of-type",
+);
+const inputElement = document.querySelector(".QuantitySelector .Input");
 
-let a = 1;
+const formElement = document.querySelector(".QuantitySelector__Form");
 
-plusItemElement.addEventListener("click", () => {
-  a++;
-  a = a < 10 ? "0" + a : a;
-  console.log(a);
-  valueItemElement.innerText = a;
+let inputValue = inputElement.value;
+
+increaseButtonElement.addEventListener("click", () => {
+  decreaseButtonElement.removeAttribute("disabled");
+  inputValue++;
+  inputElement.value = inputValue;
 });
 
-minusItemElement.addEventListener("click", () => {
-  if (a > 1) {
-    a--;
-    a = a < 10 ? "0" + a : a;
-    valueItemElement.innerText = a;
+decreaseButtonElement.addEventListener("click", () => {
+  inputValue--;
+  inputElement.value = inputValue;
+  if (inputValue <= 1) {
+    decreaseButtonElement.setAttribute("disabled", "disabled");
+  }
+});
+
+formElement.addEventListener("input", (event) => {
+  event.preventDefault();
+  if (inputElement.value <= 1) {
+    decreaseButtonElement.setAttribute("disabled", "disabled");
+  } else {
+    decreaseButtonElement.removeAttribute("disabled");
   }
 });
